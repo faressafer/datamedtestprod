7f18e18a299af62f850befd8947a4573cf01de3b
faressafer <34158695+faressafer@users.noreply.github.com>
2025-04-01 12:15:07 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: false, // ✅ Explicitly disable the App Router
-  },
 };
 
 export default nextConfig;
